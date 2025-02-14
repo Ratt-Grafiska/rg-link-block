1782a9b1eaf5ac306634e38f97503518be5fa9d1
Johan <johan@rattgrafiska.se>
2025-02-14 10:08:27 +0100

    Update index.php
    Updated plugin info and url

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 
 /**
- * Plugin Name: RG Link Group Block
- * Description: A custom block for creating a linked group of innerblocks.
- * Version: 0.1.9
+ * Plugin Name: RG Link Block
+ * Description: A custom block for creating a link to current or another page with optional URL.
+ * Version: 0.2.1
  * Author: Ratt Grafiska
- * Plugin URI: https://github.com/Ratt-Grafiska/rg-link-group-block
- * Update URI: https://github.com/Ratt-Grafiska/rg-link-group-block
+ * Plugin URI: https://github.com/Ratt-Grafiska/rg-link-block
+ * Update URI: https://github.com/Ratt-Grafiska/rg-link-block
  */
 
 if (!defined("ABSPATH")) {
