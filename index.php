9d554e0d4f1bfad60efa0d41e174238bb9e81431
Johan <johan@rattgrafiska.se>
2025-03-06 18:28:38 +0100

    Update index.php
    version bump

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: RG Link Block
  * Description: A custom block for creating a link to current or another page with optional URL.
- * Version: 0.2.2
+ * Version: 0.2.3
  * Author: Ratt Grafiska
  * Plugin URI: https://github.com/Ratt-Grafiska/rg-link-block
  * Update URI: https://github.com/Ratt-Grafiska/rg-link-block
